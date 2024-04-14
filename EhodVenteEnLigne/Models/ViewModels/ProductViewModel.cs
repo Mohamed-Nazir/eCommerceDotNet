51997a24637fc6a901b1e9f904a8108774035bb6
NaZiiru <m.naziroudiankha@gmail.com>
2024-04-14 02:30:39 +0000

    Added form validation messages for product creation fields

--- EhodVenteEnLigne/Models/ViewModels/ProductViewModel.cs
+++ EhodVenteEnLigne/Models/ViewModels/ProductViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
 
 namespace EhodBoutiqueEnLigne.Models.ViewModels
 {
@@ -7,14 +8,18 @@ namespace EhodBoutiqueEnLigne.Models.ViewModels
         [BindNever]
         public int Id { get; set; }
 
+        [Required(ErrorMessage ="Name is required")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage ="Description is required")]
         public string Description { get; set; }
 
         public string Details { get; set; }
 
+        [Required(ErrorMessage ="Stock is required")]
         public string Stock { get; set; }
-
+        
+        [Required(ErrorMessage ="Price is required")]
         public string Price { get; set; }
     }
 }
